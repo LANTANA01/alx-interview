cf04a9cb3768aad023984ca4232ebb12df6e12d5
LANTANA01 <ojimahtijani@gmail.com>
2024-08-08 21:07:19 +0000

    interview_minimumNUMB_of_operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,15 +10,15 @@ def minOperations(n):
 
     while n > 1:
         if n % divisor == 0:
-            print("divisor is", divisor)
+            #print("divisor is", divisor)
             n = n / divisor
-            print("\nn = n / divisor", n)
+            #print("\nn = n / divisor", n)
 
             num_of_operations = num_of_operations + divisor
-            print("\nnum of operations", num_of_operations)
+            #print("\nnum of operations", num_of_operations)
 
         else:
             divisor += 1
-            print("divisor after increment: ", divisor)
+            #print("divisor after increment: ", divisor)
 
     return num_of_operations
